d0dea62d6c3e288108792c6107901e91e4ce90af
Mauricio Miraglio <mauricio.miraglio@gmail.com>
2019-06-26 08:43:17 -0300

    Implementation of Login / Logout functionality

--- public/js/app.js
+++ public/js/app.js
@@ -14,11 +14,35 @@ const configureClient = async () => {
 window.onload = async () => {
     await configureClient();
     updateUI();
+
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        // show the gated content
+        return;
+    }
+
+    // Check for the code and state parameters
+    const query = window.location.search;
+    if (query.includes("code=") && query.includes("state=")) {
+        // Process the login state
+        await auth0.handleRedirectCallback();
+        updateUI();
+        // Use replaceState to redirect the user away and remove the querystring parameters
+        window.history.replaceState({}, document.title, "/");
+    }
 };
 
 const updateUI = async () => {
     const isAuthenticated = await auth0.isAuthenticated();
-
     document.getElementById("btn-logout").disabled = !isAuthenticated;
     document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+const login = async () => {
+    await auth0.loginWithRedirect({ redirect_uri: window.location.origin });
+};
+
+const logout = () => {
+    auth0.logout({ returnTo: window.location.origin });
 };
